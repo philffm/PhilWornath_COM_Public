5ad06a874248c542984027d5b484f65d2b989f45
Phil <philipp@wornath.de>
2019-09-29 23:01:19 +0200

    Updates

--- cv/wp-content/plugins/rs-card-shortcodes/shortcodes.js
+++
@@ -1,9 +0,0 @@
-jQuery(window).load(function(){
-	
-	jQuery(document).on("change", ".sc_select", function(){
-		var val =jQuery(this).val();
-				send_to_editor(val);
-				return false;
-	});
-	
-});
